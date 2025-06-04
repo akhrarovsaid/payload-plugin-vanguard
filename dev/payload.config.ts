920b74811461a7e8015b38e852b6d1cac67295db
akhrarovsaid <akhrarovsaid@gmail.com>
2025-06-04 00:44:50 -0400

    refactor: update plugin template to use turbo and mongodb-memory-server

--- dev/payload.config.ts
+++ dev/payload.config.ts
@@ -1,5 +1,6 @@
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { MongoMemoryReplSet } from 'mongodb-memory-server'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { vanguardPlugin } from 'payload-plugin-vanguard'
@@ -17,48 +18,63 @@ if (!process.env.ROOT_DIR) {
   process.env.ROOT_DIR = dirname
 }
 
-export default buildConfig({
-  admin: {
-    autoLogin: devUser,
-    importMap: {
-      baseDir: path.resolve(dirname),
-    },
-  },
-  collections: [
-    {
-      slug: 'posts',
-      fields: [
-        {
-          name: 'title',
-          type: 'text',
-        },
-        {
-          name: 'image',
-          type: 'upload',
-          relationTo: 'media',
-        },
-      ],
-    },
-    {
-      slug: 'media',
-      fields: [],
-      upload: {
-        staticDir: path.resolve(dirname, 'media'),
+const buildConfigWithMemoryDB = async () => {
+  if (process.env.NODE_ENV === 'test') {
+    const memoryDB = await MongoMemoryReplSet.create({
+      replSet: {
+        count: 3,
+        dbName: 'payloadmemory',
+      },
+    })
+
+    process.env.DATABASE_URI = `${memoryDB.getUri()}&retryWrites=true`
+  }
+
+  return buildConfig({
+    admin: {
+      autoLogin: devUser,
+      importMap: {
+        baseDir: path.resolve(dirname),
       },
     },
-  ],
-  db: mongooseAdapter({
-    url: process.env.DATABASE_URI || '',
-  }),
-  editor: lexicalEditor(),
-  email: testEmailAdapter,
-  onInit: async (payload) => {
-    await seed(payload)
-  },
-  plugins: [vanguardPlugin({})],
-  secret: process.env.PAYLOAD_SECRET || 'test-secret_key',
-  sharp,
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
-  },
-})
+    collections: [
+      {
+        slug: 'posts',
+        fields: [
+          {
+            name: 'title',
+            type: 'text',
+          },
+          {
+            name: 'image',
+            type: 'upload',
+            relationTo: 'media',
+          },
+        ],
+      },
+      {
+        slug: 'media',
+        fields: [],
+        upload: {
+          staticDir: path.resolve(dirname, 'media'),
+        },
+      },
+    ],
+    db: mongooseAdapter({
+      url: process.env.DATABASE_URI || '',
+    }),
+    editor: lexicalEditor(),
+    email: testEmailAdapter,
+    onInit: async (payload) => {
+      await seed(payload)
+    },
+    plugins: [vanguardPlugin({})],
+    secret: process.env.PAYLOAD_SECRET || 'test-secret_key',
+    sharp,
+    typescript: {
+      outputFile: path.resolve(dirname, 'payload-types.ts'),
+    },
+  })
+}
+
+export default buildConfigWithMemoryDB()
